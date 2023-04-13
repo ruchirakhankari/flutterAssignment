cc4f252d037e4eeec06c3bcf2f5e9aa7a0377902
ruchirakhankari <130677859+ruchirakhankari@users.noreply.github.com>
2023-04-13 21:14:08 +0530

    Update todo_services.dart
    service

--- lib/model/services/todo_services.dart
+++ lib/model/services/todo_services.dart
@@ -1,10 +1,9 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:flutterassignment/model/todo.dart';
-
 import '../Featured.dart';
 import '../Popular.dart';
+
 class TopService {
   Future<List<Top>> getAll() async {
     const url = 'https://jsonplaceholder.typicode.com/photos';
@@ -14,12 +13,11 @@ class TopService {
       final json = jsonDecode(response.body) as List;
       final top = json.map((e) {
         return Top(
-          albumId: e['albumId'],
-          id: e['id'],
-          title: e['title'],
-          url: e['url'],
-          thumbnailUrl:e['thumbnailUrl']
-        );
+            albumId: e['albumId'],
+            id: e['id'],
+            title: e['title'],
+            url: e['url'],
+            thumbnailUrl: e['thumbnailUrl']);
       }).toList();
       return top;
     }
@@ -27,24 +25,6 @@ class TopService {
     // throw "Something went wrong";
   }
 
-  Future<List<Featured>> getAllfeatured() async {
-    const url = 'https://dummyjson.com/comments';
-    final uri = Uri.parse(url);
-    final response = await http.get(uri);
-    if (response.statusCode == 200) {
-      final json = jsonDecode(response.body) as List;
-      final featured = json.map((e) {
-        return Featured(
-            id: e['id'],
-            body: e['body'],
-            postId: e['postId'],
-        );
-      }).toList();
-      return featured;
-    }
-    return [];
-    // throw "Something went wrong";
-  }
   Future<List<Popular>> getAllpopular() async {
     const url = 'https://picsum.photos/v2/list?page=2&limit=100';
     final uri = Uri.parse(url);
@@ -54,11 +34,11 @@ class TopService {
       final popular = json.map((e) {
         return Popular(
           id: e['id'],
-          auther: e['auther'],
+          author: e['author'],
           width: e['width'],
-          height:e['height'],
+          height: e['height'],
           url: e['url'],
-          download_url:e['download_url'],
+          download_url: e['download_url'],
         );
       }).toList();
       return popular;
@@ -66,4 +46,29 @@ class TopService {
     return [];
     // throw "Something went wrong";
   }
+
+  Future<List<Featured>> getAllfeatured() async {
+    const url = 'https://picsum.photos/v2/list';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    print("Response>>>>>>>>>${response.toString()}");
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as List;
+      final featured = json.map((e) {
+        return Featured(
+          id: e['id'],
+          author: e['author'],
+          width: e['width'],
+          height: e['height'],
+          url: e['url'],
+          download_url: e['download_url'],
+        );
+      }).toList();
+      return featured;
+    }
+    return [];
+    // throw "Something went wrong";
+  }
 }
+
+
